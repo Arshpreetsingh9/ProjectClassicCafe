8dd46c6ebdbf5faa1eb49a96bef256e74bc29f95
Arshpreet Saluja <25arshpreetsingh@gmail.com>
2019-11-19 05:55:36 -0800

    user details get inserted in access

--- src/main/java/SimpleServlet.java
+++ src/main/java/SimpleServlet.java
@@ -9,81 +9,117 @@ import java.sql.*;
 
 @WebServlet("/hello")
 public class SimpleServlet extends HttpServlet {
-/**
-*
-*/
-private static final long serialVersionUID = 1L;
+	/**
+	*
+	*/
+	private static final long serialVersionUID = 1L;
 
-
-public void doPost(HttpServletRequest request,
-HttpServletResponse response) throws
-ServletException, IOException {
-PrintWriter out = response.getWriter();
-out.println("<html><head><title>PostResults</title></head>");
-out.println("<body><h1>Here is theresults</h1>");
-out.println("<p />User Name: " +
-request.getParameter("email"));
-out.println("<p />Password: " +
-request.getParameter("password"));
-out.println("</body></html>");
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>PostResults</title></head>");
+		out.println("<body><h1>Here is theresults</h1>");
+		out.println("<p />User Name: " + request.getParameter("email"));
+		out.println("<p />Password: " + request.getParameter("password"));
+		out.println("</body></html>");
 
 // variables
-Connection connection = null;
-Statement statement = null;
-ResultSet resultSet = null;
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		ResultSet validatelogin = null;
 // Step 1: Loading or registering ucanaccess
 // JDBC driver class
-try {
-	Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-} catch (ClassNotFoundException cnfex) {
-	System.out.println("Problem in loading or " + "registering MS Access JDBC driver");
-	cnfex.printStackTrace();
-}
-try {
-	
-	String msAccDB = "C:/Temp/Employee.accdb";
-	String dbURL = "jdbc:ucanaccess://" + msAccDB;
-	// Step 2.A: Create and get connection
-	// using DriverManager class
-	connection = DriverManager.getConnection(dbURL);
-	// Step 2.B: Creating JDBC Statement
-	statement = connection.createStatement();
-
-	// Step 2.C: Executing SQL & retrieve
-	// data into ResultSet
-	resultSet = statement.executeQuery("SELECT * FROM EMPLOYEE");
-	while (resultSet.next()) {
-		int id = resultSet.getInt(1);
-		String name = resultSet.getString(2);
-		Double salary = resultSet.getDouble(3);
-		System.out.println("Employee #" + id + ": " + name + ", " + salary);
-		
-		String html = "<html>";
-		html+= "<h2>name :"+name+"<br/>";
-		html+= "</html>";
-		out.println(html);
-	
-	}
-} catch (SQLException sqlex) {
-	sqlex.printStackTrace();
-} finally {
-	// Step 3: Closing database connection
-	try {
-		if (connection != null) {
-			// cleanup resources, once
-			// after processing
-			resultSet.close();
-			statement.close();
-			// and then finally close connection
-			connection.close();
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		} catch (ClassNotFoundException cnfex) {
+			System.out.println("Problem in loading or " + "registering MS Access JDBC driver");
+			cnfex.printStackTrace();
 		}
-	} catch (SQLException sqlex) {
-		sqlex.printStackTrace();
+		try {
+			//user inputs from signup form.
+			String cust_email = request.getParameter("email");
+			String cust_password = request.getParameter("password");
+			String cust_repeatpass = request.getParameter("password-repeat");
+			int cust_reward = 1;
+			String cust_Favorites = "Not decided yet!";
+			
+			
+			
+			//String loginquery = "SELECT * FROM CUSTOMER WHERE EMAIL = '"+login_email + "' AND PASSWORD = '" + login_password+"'; ";
+			//resultSet = statement.executeQuery(loginquery);
+			
+			//Connection to database.
+			String msAccDB = "C:/Users/25ars/Documents/Employee.accdb";
+			String dbURL = "jdbc:ucanaccess://" + msAccDB;
+			// Step 2.A: Create and get connection
+			// using DriverManager class
+			connection = DriverManager.getConnection(dbURL);
+			// Step 2.B: Creating JDBC Statement
+			statement = connection.createStatement();
+
+			//Validating login data 
+			//user inputs from signin form.
+			String login_email = request.getParameter("login_email");
+			String login_password = request.getParameter("login_password") ;
+			
+			validatelogin = statement.executeQuery("SELECT * FROM CUSTOMER WHERE EMAIL = '"+login_email + "' AND PASSWORD = '" + login_password+"'; ");
+			String check_email = validatelogin.getString(2);
+			String check_password = validatelogin.getString(3);
+			if(login_email.equalsIgnoreCase(check_email) && login_password.equals(check_password)) {
+				System.out.println("All good.");
+			}else {
+				System.out.println("There's no such record.");
+			}
+
+			
+			// Adding customer info to database.
+
+			if (cust_repeatpass.equals(cust_password)) {
+				String insertSQL = "INSERT INTO CUSTOMER (EMAIL, PASSWORD, REWARDS, fAVORITES) " + "VALUES ('"
+						+ cust_email + "', '" + cust_password + "', '" + cust_reward + "', '" + cust_Favorites + "')";
+				statement.executeUpdate(insertSQL);
+
+
+//Step 2.C: Executing SQL & retrieve
+				// data into ResultSet
+				resultSet = statement.executeQuery("SELECT * FROM CUSTOMER WHERE EMAIL = '"+cust_email + "' AND PASSWORD = '" + cust_password+"'; ");
+				while (resultSet.next()) {
+					int id = resultSet.getInt(1);
+					String email = resultSet.getString(2);
+					String password = resultSet.getString(3);
+					int rewards = resultSet.getInt(4);
+					String favorites = resultSet.getString(5);
+					System.out.println(
+							"Customer #" + id + ": " + email + ", " + password + ", " + rewards + ", " + favorites);
+					String html = "<html>";
+					html += "<h2>name :" + email + "<br/>";
+					html += "</html>";
+					out.println(html);
+				}
+				
+				
+			}else {
+				System.out.println("Your passwords didn't match.");
+			}
+		} catch (SQLException sqlex) {
+			sqlex.printStackTrace();
+		} finally {
+			// Step 3: Closing database connection
+			try {
+				if (connection != null) {
+					// cleanup resources, once
+					// after processing
+					resultSet.close();
+					statement.close();
+					// and then finally close connection
+					connection.close();
+				}
+			} catch (SQLException sqlex) {
+				sqlex.printStackTrace();
+			}
+		}
+
+		out.flush();
+		out.close();
 	}
-}
-
-
-out.flush();
-out.close();
-}
 }
